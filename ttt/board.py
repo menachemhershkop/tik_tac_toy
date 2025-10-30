de44f2a45329eba4ce84471800564f5ea5e49fa2
mendy hershkop <hers.mm@gmail.com>
2025-10-30 15:48:38 +0200

    create board

--- ttt/board.py
+++ ttt/board.py
@@ -1,1 +1,6 @@
-ECHO is on.
+def create_board(size : int=3):
+    return [["_" for i in range(size)]for i in range(size)]
+
+def split_board(board):
+
+    return "\n".join([" ".join(i) for i in board])
